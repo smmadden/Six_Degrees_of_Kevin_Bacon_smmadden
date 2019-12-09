5c01a8d6e489295e0091174ea643d8b327d90681
smmadden <smmadden10@gmail.com>
2019-12-08 17:19:13 -0800

    Completed implementation of graph

--- CS245A2.java
+++ CS245A2.java
@@ -1,5 +1,113 @@
-public class CS245A1{
-  public static void main(String[] args){
-    System.out.print("Initial commit");
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+// move some of this into a separate class please this is a lot
+// separate class = the graph
+// separate function = reading the file
+
+
+public class CS245A2{
+
+  public CS245A2(){
+    System.out.println("Starting program: \nAccessing file...");
   }
+
+  public static void main(String[] args){
+    GraphImplementation graph = new GraphImplementation();
+    CS245A2 program = new CS245A2();
+    File file = new File("/Users/saigemadden/documents/sem3/cs245/assignment2/tmdb_5000_credits.csv");
+
+    int line_num = 0;
+    try{
+      LinkedList<LinkedList<String>> moviesAndActors = new LinkedList<>();
+      int totalNumActors = 0;
+      Scanner scan = new Scanner(file);
+      String line;
+
+      scan.nextLine(); // skips over first line which has formatting
+      line_num++;
+      while(scan.hasNextLine()){
+      //for(int i=0; i<400; i++){
+        line = scan.nextLine();
+        line_num++;
+        // split line up based on info we want - how should I do this?
+        //String delim = "[";
+        //String[] line_arr = line.split();
+        line = line.substring(line.indexOf(",")+1);
+        line = line.substring(line.indexOf(",")+1); // now should just be cast and crew
+        String cast = line.substring(line.indexOf("["), line.indexOf("]")); // this gives us all of the cast
+        String[] actors = cast.split("\\},");
+        //System.out.println(cast + "\n\n");
+        //System.out.println(actors[0] + "\n\n");
+        // members now has each character's info
+        if(cast.length() > 1){ // if the cast length is one, that means the only character is [
+          //LinkedList<String> movie = new LinkedList<>(); // all the ones that the edges need to be added for
+          for(int j=0; j<actors.length; j++) {
+            int index = actors[j].indexOf("name");
+            if(index != -1){
+              actors[j] = actors[j].substring(index, actors[j].indexOf("\"\", \"\"order"));
+              actors[j] = actors[j].substring(actors[j].lastIndexOf("\"") + 1); // shaves off ""
+              graph.addVertex(actors[j]);
+              //movie.add(0, actors[j]);
+            }
+          }
+
+          //moviesAndActors.add(0, movie);
+          //System.out.println(actors[j]);
+          totalNumActors += actors.length;
+
+
+
+          // now to add the edges
+          // could do a nested for loop - is there a way that is more efficient?
+          // only adding with the first one because the indexes aren't referring to the graph but to the actors
+          for(int z=0; z<actors.length; z++){
+            for(int c=0; c<actors.length; c++){
+              if(z != c){ // if z == c then don't want to add the edge because would be adding a connection to itself
+                try{
+                  graph.addEdge(graph.findValue(actors[z]), graph.findValue(actors[c])); // know that the vertices exist so no reason to worry about findValue returning negative one
+                } catch(Exception e){
+                  System.out.println("\n\n\nProblem: line " + line_num + "\n " + actors[z] + " " + actors [c] + "\n\n\n");
+                }
+              }
+
+            }
+          }
+
+
+
+
+        }
+
+
+        //System.out.println(moviesAndActors.get(0).get(0)); // backwards, so will be the last actor of the last movie // right now will print out same thing twice is cast empty
+
+        //System.out.println("\n" + totalNumActors + "\n\n");
+      }
+
+       // backwards, so will be the last actor of the last movie
+      // now have a count of all of the actors - not really but should be easy
+      // now have a collection of who is in which movie with whom and also have an index associated with the people
+
+
+
+    } catch(Exception e){
+      System.out.println("Error reading file " + line_num + e.getMessage());
+      e.printStackTrace();
+      return;
+    }
+
+    graph.print();
+
+    Scanner input = new Scanner(System.in);
+
+//    System.out.print("Actor 1 name: ");
+//    String actor1 = input.nextLine();
+//    System.out.print("Actor 2 name: ");
+//    String actor2 = input.nextLine();
+//    System.out.println("Path between " + actor1 + " and " + actor2 + ": ");
+
+  }
+
 }
---
+++ GraphImplementation.java
@@ -0,0 +1,93 @@
+import java.util.*;
+
+public class GraphImplementation {
+    // adjacency list implementation of a graph to store the actors
+
+    private ArrayList<GraphNode> graph;
+    private int vertices;
+    // don't need number of vertices because length of graph will be updated as needed
+
+    public GraphImplementation(){
+        graph = new ArrayList<>();
+        vertices = 0;
+    }
+
+    public void addVertex(String value){ // the head of the list is the vertex at that index
+        // if the same value is added twice don't do anything
+        if(findValue(value) == -1){
+            graph.add(new GraphNode(value, vertices++));
+            //vertices++;
+        }
+
+    }
+
+    public void addEdge(int src, int tar) throws Exception {
+        // throw an exception if graph is empty or only has one value (bc not two vertices to make an edge for) or if the vertex is out of bounds
+        if(graph.size() <= 1 || src >= vertices || src < 0 || tar >= vertices || tar < 0){
+            throw new Exception();
+        }
+        // this is an undirected graph so an edge will be added to both the src and the tar
+        GraphNode source = graph.get(src);
+        GraphNode target = graph.get(tar);
+
+        // source and target could never be null - not possible because wouldn't be a vertex if so
+
+        while(source.next != null){
+            source = source.next;
+        }
+        while(target.next != null){
+            target = target.next;
+        }
+        source.next = new GraphNode(graph.get(tar));
+        target.next = new GraphNode(graph.get(src));
+
+    }
+
+    public void print(){
+        for(int i=0; i<vertices; i++){
+            GraphNode node = graph.get(i);
+            while(node != null){
+                System.out.print(node.value + ", ");
+                node = node.next;
+            }
+            System.out.println();
+        }
+        System.out.println(vertices);
+
+    }
+
+    // do we need a function for neighbors? is just the list of nodes at a particular index
+
+    public int findValue(String value){
+        for(int i=0; i<vertices; i++){
+            if(graph.get(i).value.equals(value)){
+                return i;
+            }
+        }
+        // finding a particular value, in this case an actor, in the list
+        return -1; // value not found
+    }
+
+
+
+    class GraphNode{
+        private String value;
+        private int index; // to keep track of the index of the actor in the list instead of doing a find each time
+        private GraphNode next;
+
+        public GraphNode(String value, int index){
+            this.value = value;
+            this.index = index;
+            next = null;
+        }
+
+        public GraphNode(GraphNode node){ // creates a copy of the node that is a parameter - easier to add edges this way
+            value = node.value;
+            index = node.index;
+            next = null;
+        }
+
+        // getters and setters
+
+    }
+}
